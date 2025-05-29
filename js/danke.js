e22e9a2de8069f5ce9936ee6c79eabebcd871fda
Mandoquest <norwin@yandaal.de>
2025-05-29 19:04:27 +0200

    iuggig

--- js/danke.js
+++ js/danke.js
@@ -1,11 +1,16 @@
 function ZeigeMenu() {
   const menu = document.getElementById("Menu");
+  const flexbox = document.getElementById("Flexbox");
+
   const isOpen = menu.classList.contains("Menu-offen");
+
   if (isOpen) {
     // Menü schließen → Inhalt wieder zeigen
     menu.classList.remove("Menu-offen");
+    flexbox.style.display = "flex";
   } else {
     // Menü öffnen → Inhalt ausblenden
     menu.classList.add("Menu-offen");
+    flexbox.style.display = "none";
   }
 }
